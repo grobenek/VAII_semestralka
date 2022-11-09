da114c42c2a6a0652db83d1ad7c73a8a789f7840
Peter Szathmary <peter.szathmary01@gmail.com>
2022-11-09 08:47:11 +0100

    404 page created

---
+++ error_page/my_404.php
@@ -0,0 +1,11 @@
+
+<?php
+
+require "../components/head.php";
+require "../components/header.php" ?>
+
+<h1>Error 404</h1>
+<h2>There was problem on website</h2>
+<h3>Please contact support!</h3>
+</body>
+</html>
